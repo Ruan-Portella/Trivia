0c5563272928cee5a325d2ac5531d42fd10fa22a
Ruan-Portella <ruanmorales29@gmail.br>
2023-04-19 17:09:12 -0300

    requisito 03
    Criei a Rota Settings, e um botão para mandar.

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <h1 data-testid="settings-title">Settings</h1>
+    );
+  }
+}
