d148ca1bcbb6b6da336903d470549aa036655b23
Ruan-Portella <ruanmorales29@gmail.br>
2023-04-20 00:24:46 -0300

    Resolvendo Conflitos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import Routes from './components/Routes';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import logo from './trivia.png';
+import './App.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Routes />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>SUA VEZ</p>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/game" component={ Game } />
+        </Switch>
+      </main>
+    </div>
+  );
 }
