a241bc539f57bb29c7a321b7ed46683bfd651fff
Lívio Lopes <antonio.lvio760@gmail.com>
2023-04-20 15:53:36 -0300

    Requisito 18
    implementado botao para redirecionar para pagina de login

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,31 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Ranking extends Component {
+  goHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div>Ranking</div>
+      <div>
+        Ranking
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.goHome }
+        >
+          Inicio
+
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
