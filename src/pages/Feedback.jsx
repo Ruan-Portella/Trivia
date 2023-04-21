890a22decfd802b045e2856c93d619414d69bba6
Ruan-Portella <ruanmorales29@gmail.br>
2023-04-21 13:16:35 -0300

    Requisito 13
    Feat: Criei a logica de rendezirar as mensagens

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -4,15 +4,34 @@ import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 
 class Feedback extends Component {
+  state = {
+    message: '',
+  };
+
+  componentDidMount() {
+    const QuestionsAssertion3 = 3;
+    const { assertions } = this.props;
+    if (assertions < QuestionsAssertion3) {
+      this.setState({
+        message: 'Could be better...',
+      });
+    } else if (assertions >= QuestionsAssertion3) {
+      this.setState({
+        message: 'Well Done!',
+      });
+    }
+  }
+
   render() {
     const { name, score, gravatarEmail } = this.props;
+    const { message } = this.state;
     const picture = md5(gravatarEmail).toString();
     return (
       <section>
         <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${picture}` } alt="" />
         <p data-testid="header-player-name">{ name }</p>
         <span data-testid="header-score">{ score }</span>
-        <p data-testid="feedback-text">Feedback</p>
+        <p data-testid="feedback-text">{message}</p>
       </section>
     );
   }
@@ -22,12 +41,14 @@ const mapStateToProps = (state) => ({
   name: state.player.name,
   score: state.player.score,
   gravatarEmail: state.player.gravatarEmail,
+  assertions: state.player.assertions,
 });
 
 Feedback.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   gravatarEmail: PropTypes.string.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
