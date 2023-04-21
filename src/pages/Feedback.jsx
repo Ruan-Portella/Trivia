a4c1b94ab48f2a2f409e5bb7955d2dbc09cb50ea
Ruan-Portella <ruanmorales29@gmail.br>
2023-04-21 12:03:40 -0300

    Requisito 11
    Cria a tela de feedback para passar na 11

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <p data-testid="feedback-text">Feedback</p>
+    );
+  }
+}
