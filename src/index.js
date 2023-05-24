a4d3c212a72e89fefd674a5c376e0abf1f83fe2b
Ruan-Portella <ruanmorales29@gmail.br>
2023-05-23 22:14:09 -0300

    Feat: Configura gh pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
